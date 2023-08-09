8ccf20263aa6b7a72916908667efdb4812172421
Victor Manuel - Softtek <victorm.vazquez@softtek.com>
2023-08-09 13:53:43 -0600

    Creación de nuevos archivos para clase 1 y 2

---
+++ JavaScript/condicionales.js
@@ -0,0 +1,12 @@
+console.warn("Lo siguiente se encuenta su string en <head>");
+
+a = "valor string";
+b = 239;
+c = false;
+d = "valor numerico";
+b = d;
+console.log(`El valor de ${b} se nombra en el valor ${d}`);
+e = typeof(c)
+if(e = Boolean){
+    console.log(`El valor de c es ${c} y es boolean`);
+}
