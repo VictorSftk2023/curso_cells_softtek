04b69c0e3b34914292aad6c9f010499edec77073
Victor Manuel - Softtek <victorm.vazquez@softtek.com>
2023-08-11 00:03:46 -0600

    Agregando mas ejemplos y ordenando archivos js_2

---
+++ JavaScript/booleans.js
@@ -0,0 +1,19 @@
+console.error("Ejercicios de valores booleanos ")
+
+let booleano_1 = true, booleano_2 = false;
+console.log(`Los unicos valores booleanos son: ${booleano_1} y ${booleano_2}`);
+console.log(booleano_1, booleano_2);
+
+// Confirmacion de booleanos con condicional
+let error = false;
+if (error) {
+    console.log("Es true")
+}else{
+    console.log("Es false")
+}
+/*El resultado no da nada, ya que la condicional "if" siempre esta 
+    evaluando un valor que se cumple (o sea ture).
+    Y al declarar ese true como false, ejecuta el "else" al no cumplir con "if"*/
+
+
+
